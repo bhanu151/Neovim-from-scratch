ee4740c5d29bb9d38f0ad54399c8f726695f0e52
Bhanu Priya <bhanupriya151@gmail.com>
2022-03-27 01:02:38 +0530

    Gkeep and Jukit added

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -99,6 +99,14 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
 
+  -- Google Keep
+  use {
+    "stevearc/gkeep.nvim",
+    run = ":UpdateRemotePlugins"
+  }
+  --Jukit
+  use "luk400/vim-jukit"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
