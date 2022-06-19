6c2cebafa4b520a6b89f3d3ed15925ad520f85bd
Bhanu Priya <bhanupriya151@gmail.com>
2022-06-19 12:27:20 +0530

    auto open and close of dapui with dap

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -9,3 +9,16 @@ table.insert(require('dap').configurations.python, {
 
 require('dapui').setup()
 require('nvim-dap-virtual-text').setup()
+
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+
+dap.listeners.after.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+
+dap.listeners.after.event_exited["dapui_config"] = function()
+  dapui.close()
+end
