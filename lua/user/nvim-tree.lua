5eacbf721328db279c26e3689ad64aef8dc64641
Bhanu Priya <bhanupriya151@gmail.com>
2023-09-29 12:23:22 +0530

    nvim-tree | plugin update

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,33 +1,10 @@
--- following options are the default
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
-
-local tree_cb = nvim_tree_config.nvim_tree_callback
-
-local nvim_tree_options = {
-  nvim_tree_git_hl = 1,
-  root_folder_modifier = ":t",
-  nvim_tree_show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
-  },
-  nvim_tree_respect_buf_cwd = 1,
-}
-
-for k, v in pairs(nvim_tree_options) do
-  vim.g.k = v
-end
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 local function on_attach(bufnr)
   local api = require("nvim-tree.api")
@@ -40,12 +17,16 @@ local function on_attach(bufnr)
       nowait = true,
     }
   end
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+  -- custom mappings
   vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
   vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
   vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+  vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
 end
 
-nvim_tree.setup {
+nvim_tree.setup({
   on_attach = on_attach,
   renderer = {
     icons = {
@@ -70,12 +51,8 @@ nvim_tree.setup {
         },
       },
     },
+    root_folder_label = false,
   },
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
   diagnostics = {
     enable = true,
     icons = {
@@ -94,10 +71,6 @@ nvim_tree.setup {
     cmd = nil,
     args = {},
   },
-  filters = {
-    dotfiles = false,
-    custom = {},
-  },
   git = {
     enable = true,
     ignore = true,
@@ -105,11 +78,7 @@ nvim_tree.setup {
   },
   view = {
     adaptive_size = true,
-    hide_root_folder = false,
     side = "right",
-    mappings = {
-      custom_only = false,
-    },
     number = false,
     relativenumber = false,
   },
@@ -117,13 +86,4 @@ nvim_tree.setup {
     cmd = "trash",
     require_confirm = true,
   },
-  actions = {
-    open_file = {
-      quit_on_open = false,
-      window_picker = {
-        enable = false,
-      },
-      resize_window = true,
-    },
-  },
-}
+})
